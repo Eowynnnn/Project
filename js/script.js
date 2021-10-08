c87995160713adb03cd754a8095656e14d1a8073
Bohdan <vivivivibnktrch238@ukr.net>
2021-10-08 20:11:27 +0200

    first practik

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 "use strict";
 
-console.log('arr'+ '-object')
-console.log(4+ +"5");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+
+const a = prompt("Какой последний фильм вы посмотрели?", ""),
+    b = prompt("На сколько оцените его ?", ""),
+    c = prompt("Какой последний фильм вы посмотрели?", ""),
+    d = prompt("На сколько оцените его ?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
+
+
